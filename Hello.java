7b1b6845e067eecf3cebb5942c6ceb9cf9335884
balabanbetul <101840960+balabanbetul@users.noreply.github.com>
2022-07-29 22:34:18 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World from GitHub Webhook!"); 
-        System.out.println("Hello World from GitHub Webhook!"); 
-         System.out.println(" Webhook!"); 
-        System.out.println(" Webhook!");
-        System.out.println(" Webhook!");
+     
     }
 }
